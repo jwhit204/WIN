1a0d83d507396948742fda76a8b3c6e394d31d32
Jharna Whitehurst <jharnawhitehurst@Jharnas-MBP.attlocal.net>
2021-03-26 14:13:31 -0400

    group project react without create react app

---
+++ 6.5/Webpack/javascript/index.js
@@ -0,0 +1,9 @@
+// index.js
+// require("style-loader!css-loader!../css/app.css");
+// app/javascript/index.js
+
+require("../css/app.css");
+
+require('./another.js');
+  
+sayHello();
---
+++ 6.5/Webpack/webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    entry: {
+      app: "./javascript/index"
+    },
+    output: {
+      path: __dirname + '/dist',
+      publicPath: '/',
+      filename: 'bundle.js'
+    }
+
+  };
+
