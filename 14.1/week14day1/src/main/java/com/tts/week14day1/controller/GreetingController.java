4c273d9f782b425ef87cd2d4d8ffd581aa680106
jharnawhitehurst <jharna.whitehurst@gmail.com>
2021-05-17 14:57:33 -0400

    14.1 Intro to MVC, Maven & ThymeLeaf

--- 14.1/week14day1/src/main/java/com/tts/week14day1/controller/GreetingController.java
+++ 14.1/week14day1/src/main/java/com/tts/week14day1/controller/GreetingController.java
@@ -1,22 +1,21 @@
+
 package com.tts.week14day1.controller;
 
+import com.tts.week14day1.model.Greeting;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 // @Controller is an extension of @Component
-// this means spring will pick it up  and utilize the component
-// @Controller does not  automatically have every method's return
+// this means spring will pick it up and utilize the component
+// @Controller does not automatically have every method's return
 // act as a response body -- this is only true for @RestController
 @Controller
 public class GreetingController {
 
     @GetMapping("/hello")
-    @ResponseBody // this annotation tells  spring to use the return as a response
-    public  String helloWorld() {
+    @ResponseBody // this annotation tells spring to use the return as a response
+    public String helloWorld() {
         return "hello world!";
     }
 
@@ -27,22 +26,22 @@ public class GreetingController {
     // please remember that without the @ResponseBody
     // this method will only work with our HTML files in the templates folder
     @GetMapping("/greeting")
-    public String  greeting(@RequestParam(name="name",
+    public String greeting(@RequestParam(name="name",
             required = false,
             defaultValue="World") String name, Model model) {
-        model.addAttribute("randomValue",452352L);
-        model.addAttribute ("name", name);
+        model.addAttribute("randomValue", 452352L);
+        model.addAttribute("name", name);
         return "greeting";
     }
 
-    // the path variable here will  be part of the endpoint
+    // the path variable here will be part of the endpoint
     @GetMapping("/greeting/var/{name}")
     @ResponseBody
-    public String greetingWithPathVar(@PathVariable  String name) {
+    public String greetingWithPathVar(@PathVariable String name) {
         return "hello there, " + name;
     }
 
-    // the request param here is not considered part of your  endpoint
+    // the request param here is not considered part of your endpoint
     // think of it more as an extension of your endpoint
     @GetMapping("/greeting/param/")
     @ResponseBody
@@ -51,4 +50,17 @@ public class GreetingController {
             defaultValue="World") String name) {
         return "hello there, " + name;
     }
+
+    @PostMapping("/greeting")
+    public String greetingSubmit( @ModelAttribute Greeting greeting, Model model) {
+        model.addAttribute("greeting", greeting);
+        return "result";
+    }
+
+    @ModelAttribute("greeting")
+    public Greeting messages() {
+        return new Greeting();
+    }
+
+
 }
