85578a6e3384fac1b531a290d4c359bf7ac5d157
jharnawhitehurst <jharna.whitehurst@gmail.com>
2021-05-18 10:22:13 -0400

    14.2 OAuth

---
+++ 14.2/authentication/src/main/java/com/tts/authentication/configuration/MvcConfig.java
@@ -0,0 +1,21 @@
+package com.tts.authentication.configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    // This method allows us to add view controllers  to our application
+    // the addViewController method defines our path
+    // the setViewName method sets that path to a template
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
