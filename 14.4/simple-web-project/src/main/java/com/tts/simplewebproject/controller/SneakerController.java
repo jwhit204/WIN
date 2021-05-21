b460823b8173fadb5cd844683953fb47e22e0fe8
jharnawhitehurst <jharna.whitehurst@gmail.com>
2021-05-21 08:16:10 -0400

    14.4  Simple Web Project

---
+++ 14.4/simple-web-project/src/main/java/com/tts/simplewebproject/controller/SneakerController.java
@@ -0,0 +1,37 @@
+package com.tts.simplewebproject.controller;
+
+import com.tts.simplewebproject.model.Sneaker;
+import com.tts.simplewebproject.repository.SneakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+//@Controller
+public class SneakerController {
+
+    @Autowired
+    SneakerRepository sneakerRepository;
+    @GetMapping("/")
+ //   @ResponseBody
+    public String returnHelloWorld(){
+        System.out.println("This will work");
+        return "Hello world";
+    }
+
+    @GetMapping("/sneaker")
+    public Sneaker returnNewSneaker() {
+        return new Sneaker(2L, "blue");
+    }
+
+    @GetMapping("/all")
+    public Iterable<Sneaker> getAllSneakers() {
+        return  sneakerRepository.findAll();
+    }
+
+    @PostMapping("/add")
+    public Sneaker addSneaker(@RequestBody Sneaker sneaker) {
+        return sneakerRepository.save(sneaker);
+    }
+
+}
---
+++ 14.4/simple-web-project/src/main/java/com/tts/simplewebproject/model/Sneaker.java
@@ -0,0 +1,47 @@
+package com.tts.simplewebproject.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+// every class inherits methods from the parent class object
+@Entity
+public class Sneaker {
+
+    // here we're using long
+    // because it allows to store a large amount of IDs
+    // its the wrapper version of long
+    // we have access to helper methods that live on Long.java
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String color;
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Sneaker{" +
+                "id=" + id +
+                ", color='" + color + '\'' +
+                '}';
+    }
+
+    public Sneaker(Long id, String color) {
+        this.id = id;
+        this.color = color;
+    }
+
+    public Sneaker() {}
+}
